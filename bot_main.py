8f69b21c6817b5bf8e7cf1c2ed197698eba6e6d8
George Ye <george.ye@gtri.gatech.edu>
2020-10-04 03:43:49 -0400

    Fixing deployment

--- bot_main.py
+++ bot_main.py
@@ -4,9 +4,7 @@ import time
 twitter_bot = TwitterBot()
 
 while True:
-    print("in loop")
-    twitter_bot.status_update()
-    twitter_bot.retweet()
-    time.sleep(5)
+    if twitter_bot.status_update():
+        twitter_bot.retweet()
 
 
